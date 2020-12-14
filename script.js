12adaf8cef23fb6696a8bcedcde3dc9e66da77c9
Svetlana <svetlana.pelevina@truesalessoft.com>
2020-12-14 10:51:05 +0300

    init9

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let lexOrigin = 'https://vsdev-developer-edition.na88.force.com/Napili/'
 
-function send() {
+const send = () => {
 	let message1 = document.getElementById("message").value;
     parent.postMessage(message1, lexOrigin);
 }
@@ -11,3 +11,11 @@ const getData = () => {
 		alert(response);
 	});	
 }
+
+const modifyText = (event) => {
+	document.getElementById("receive").value = event.data + ' ' + event.lexOrigin;
+    alert(event.data);
+    alert(event.lexOrigin);	
+}
+
+winsow.addEventListener("message", modifyText.bind(this));
